2c4eaba5d3d730bf4a4e2e3ffe7e370c78080f5b
Marcel Williams <truu2u@yahoo.com>
2025-02-01 16:37:13 -0500

    b

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const headers = new Headers();
 headers.append("Content-Type", "application/json");
